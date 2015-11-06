8409988a6fd2b11973f5b8917bc8dc0b972d80d9
Squirrel <kanade123321@gmail.com>
2015-11-06 14:07:27 +0800

    define testing code

--- Makefile
+++ Makefile
@@ -1,14 +1,23 @@
+CXX=g++
 CXXFLAG=-Wall -std=c++11
 
-hw1: mystring.o main.o
-	g++ -o $@  $(CXXFLAG) $^
+.PHONY: hw1 test
+
+hw1: MAKEFLAG=$(CXXFLAG)
+test: MAKEFLAG= -DSELF_TEST $(CXXFLAG) 
+
+hw1: clean editor
+test: clean editor
+
+# exectable
+editor: mystring.o main.o
+	$(CXX) -o $@  $(MAKEFLAG) $^
 
 main.o: main.cpp
-	g++ $(CXXFLAG) -c $<
+	$(CXX) $(MAKEFLAG) -c $<
 
 mystring.o: mystring.cpp mystring.h
-	g++ $(CXXFLAG) -c $<
+	$(CXX) $(MAKEFLAG) -c $<
 
-clean:
-	rm -f *.o
-	rm -f hw1
+clean: 
+	rm -f *.o rm -f editor
--- main.cpp
+++ main.cpp
@@ -8,32 +8,35 @@ using std::endl;
 
 int main(int argc, char const* argv[])
 {
-    char test[] = "qazwsxedc";
+#ifndef SELF_TEST
+    cout << "hello, world" << endl;
+#else
+    char test[] = "qazwsxedcqazwsxedc";
     char target[] = "qaz";
-    char dst[] = "111";
+    char dst[] = "1121";
     mystring str, str2, str3;
-    str.set(test, 9);
+    str.set(test, 18);
     str2.set(target, 3);
-    str3.set(dst, 3);
+    str3.set(dst, 4);
 
-    cout << str << endl;
-    cout << str2 << endl;
-    cout << str3 << endl;
-
-    assert(str.frequency(str2) == 1);
+    assert(str.frequency(str2) == 2);
     assert(str.frequency(str3) == 0);
 
+    mystring rem;
+    char r[] = "wsxedcwsxedc";
+    rem.set(r, 12);
+
+    assert(str.remove(str2) == rem);
+
     mystring rep;
-    char r[] = "wsxedc";
-    rep.set(r, 6);
-
-    cout << str.remove(str2) << endl;
-    cout << str.remove(str3) << endl;
-    assert(str.remove(str2) == rep);
-
+    char rr[] = "1121wsxedc1121wsxedc";
+    rep.set(rr, 20);
     mystring str4;
-    str4.set(test, 9);
-    cout << str4 << endl;
-    cout << str4.replace(str2, str3) << endl;
+    str4.set(test, 18);
+    str4.replace(str2, str3);
+    assert(rep == str4);
+
+    cout << "All test passed!" << endl;
     return 0;
+#endif
 }
