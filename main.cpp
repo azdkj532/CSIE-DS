71da6372ea9e4bc9d92f1034a93bb52902af0f4f
Squirrel <kanade123321@gmail.com>
2015-11-25 12:27:01 +0800

    rewrite judge block

--- main.cpp
+++ main.cpp
@@ -11,7 +11,7 @@ int main(int argc, char *argv[]) {
     Client* empty_job = new Client(0, 0, 0);
     data.push_back(empty_job);  // empty jobs
     queue.push_back(empty_job);
-    Client* empty_job = new Client(0, 0, 0);
+    empty_job = new Client(0, 0, 0);
     data.push_back(empty_job);  // empty jobs
     queue.push_back(empty_job);
 
@@ -30,26 +30,23 @@ int main(int argc, char *argv[]) {
         } else {
             Client* least = queue.back();
             queue.pop_back();
-            client->set_start_time(queue.back()->departure_time());
-            if (client->deadline_test(queue.back()->departure_time()) &&
-                    least->deadline_test(client->departure_time())) {
-                // clien cutin the last client
+            if (client->deadline_test(queue.back()->departure_time()) && 
+                    least->deadline_test(
+                        std::max(client->arrival_time(), queue.back()->departure_time()) + client->service_time())) {
+                // new client cut in to the queue, both least client and it meet their deadline
                 client->set_start_time(queue.back()->departure_time());
                 queue.push_back(client);
-                least->set_start_time(queue.back()->departure_time());
-                queue.push_back(least);
             } else {
-                // what ever he does wont meet the deadline,
-                // so the client leave.
+                // client leave
                 client->set_start_time(Client::MAX_TIME);
-                least->set_start_time(queue.back()->departure_time());
-                queue.push_back(least);
             }
+            least->set_start_time(queue.back()->departure_time());
+            queue.push_back(least);
         }
     }
     for (auto it : data) {
-        //std::cout << it->_arrival_time << " " << it->_service_time << " ";
-        //std::cout << it->departure_time() << std::endl;
+        std::cout << it->arrival_time() << " " << it->service_time() << " ";
+        std::cout << it->departure_time() << std::endl;
     }
     for (auto it : data) {
         delete it;
