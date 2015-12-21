a0cbde59eb29900c0af90dc89f8a3bf3d6d90631
Squirrel <kanade123321@gmail.com>
2015-12-21 16:09:24 +0800

    create node

--- bst.h
+++ bst.h
@@ -13,16 +13,29 @@ class BSTNode {
     T _value;
     size_t _left;
     size_t _right;
+    static  const size_t MASK = ~(0x1);
 
  public:
-    BSTNode<T> * leftChild() {return static_cast<BSTNode<T>*>(_left); }
-    BSTNode<T> * rightChild() {return static_cast<BSTNode<T>*>(_right); }
-    BSTNode<T> * leftChild(BSTNode<T> * newPtr) { _left = static_cast<size_t>(newPtr); return this; }
-    BSTNode<T> * rightChild(BSTNode<T> * newPtr) { _right = static_cast<size_t>(newPtr); return this; }
+    BSTNode<T> * leftChild() {return static_cast<BSTNode<T>*>(_left & MASK); }
+    BSTNode<T> * rightChild() {return static_cast<BSTNode<T>*>(_right & MASK); }
+    BSTNode<T> * leftChild(BSTNode<T> * newPtr) {
+        _left = static_cast<size_t>(newPtr) | (_left & 1);
+        return this;
+    }
+    BSTNode<T> * rightChild(BSTNode<T> * newPtr) {
+        _right = static_cast<size_t>(newPtr) | (_right & 1);
+        return this;
+    }
+
+    bool isRightThread() { return _right & 1; }
+    bool isLeftThread() { return _left & 1; }
+    inline bool isLeaf() { return isLeftThread() && isRightThread(); }
 };
 
 template<typename T>
 class BSTree {
     BSTNode<T> * _root;
+
+ public:
 };
 #endif  // BST_H_
